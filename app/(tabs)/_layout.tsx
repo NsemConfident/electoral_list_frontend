8569c3bdfbf6d16e5e2765be657767bf0dc24c55
unknown <njocknsem@gmail.com>
2025-06-10 19:22:29 +0100

    top bar customization

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,13 +3,14 @@ import { ProtectedRoute } from "../../components/ProtectedRoute";
 import Feather from '@expo/vector-icons/Feather';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import { View } from "react-native";
+import { Link } from "expo-router";
 
 export default function TabsLayout() {
   return (
     <ProtectedRoute>
       <Tabs
       screenOptions={{ 
-        
         tabBarActiveTintColor: 'teal',
         tabBarStyle: {position: 'absolute'},
        }}
@@ -18,7 +19,19 @@ export default function TabsLayout() {
         name="index" 
         options={{ 
           title: "Home",
-          tabBarIcon: ({color}) => <Feather name="home" size={24} color={color} />
+          headerStyle: {
+            backgroundColor: Colors.primary,
+          },
+          headerTintColor: Colors.light,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: ()=> (
+            <View>
+              <Link href='/app/(tabs)/index.tsx'><Ionicons name="notifications" size={24} color="black" /></Link>
+            </View>
+          ),
+          tabBarIcon: ({color}) => <Feather name="home" size={24} color={color}/>,
          }}
            />
         <Tabs.Screen 
